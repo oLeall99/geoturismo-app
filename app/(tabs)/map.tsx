5f931afda1dbf0edd244dff01626a09b501e5ff6
arthur <art5mussi@outlook.com>
2025-10-08 22:34:11 -0300

    feat: adicionado visualização de mapa com google maps

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,10 +1,11 @@
 import CustomIconButton from '@/components/CustomIconButton';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function MapScreen() {
   return (
     <View style={styles.container}>
-      <Text>Mapa</Text>
+      <MapView style={styles.map} />
       <View style={styles.button}>
         <CustomIconButton icon="plus" color="black" />
       </View>
@@ -17,6 +18,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  map: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
   },
   button: {
     position: 'absolute',
