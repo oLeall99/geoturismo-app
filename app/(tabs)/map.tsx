6d4b8434f6f01df92efdeadc1f0bb06662a322b4
arthur <art5mussi@outlook.com>
2025-10-08 22:18:54 -0300

    feat: adicionado novo componente CustomIconButton

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,9 +1,26 @@
-import { Text, View } from 'react-native';
+import CustomIconButton from '@/components/CustomIconButton';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function MapScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={styles.container}>
       <Text>Mapa</Text>
+      <View style={styles.button}>
+        <CustomIconButton icon="plus" color="black" />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    position: 'absolute',
+    bottom: 25,
+    right: 25,
+  },
+});
