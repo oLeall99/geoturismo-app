86db00bdebae62e3187cc1a32dd3516cd5e078e9
arthur <art5mussi@outlook.com>
2025-10-08 22:03:18 -0300

    feat: adicionado eslint e prettier para padronizar e formatar projeto

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,17 +3,17 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 export default function TabLayout() {
-    return (
-        <Tabs
-            screenOptions={{
-                headerShown: false,
-                tabBarStyle: { overflow: 'visible' },
-            }}
-            tabBar={(props) => <CustomTabBar {...props} />}
-        >
-            <Tabs.Screen name="map" options={{ title: 'Mapa' }} />
-            <Tabs.Screen name="search" options={{ title: 'Buscar' }} />
-            <Tabs.Screen name="profile" options={{ title: 'Perfil' }} />
-        </Tabs>
-    );
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { overflow: 'visible' },
+      }}
+      tabBar={(props) => <CustomTabBar {...props} />}
+    >
+      <Tabs.Screen name="map" options={{ title: 'Mapa' }} />
+      <Tabs.Screen name="search" options={{ title: 'Buscar' }} />
+      <Tabs.Screen name="profile" options={{ title: 'Perfil' }} />
+    </Tabs>
+  );
 }
